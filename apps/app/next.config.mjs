74c032cdd6c7bd0089c506044b1d3bc30018b8b2
adambarito <adam@barely.io>
2024-02-21 17:40:07 -0500

    Bump node-version in package.json.

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -1,5 +1,3 @@
-// import "./src/envy.mjs";
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -39,10 +37,6 @@ const config = {
 
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-
-  env: {
-    // DATABASE_URL: process.env.DATABASE_URL ?? "",
-  },
 };
 
 export default config;
