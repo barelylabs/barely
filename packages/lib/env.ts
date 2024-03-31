596f7e66e8c947d4557550adb0597809cfdd1043
adambarito <adam@barely.io>
2024-03-31 18:47:04 -0400

    minimized scope for db env

--- packages/lib/env.ts
+++ packages/lib/env.ts
@@ -3,7 +3,6 @@ import { zEnv } from '@barely/env';
 export const { env, clientEnv } = zEnv({
 	serverEnvKeys: [
 		'AWS_S3_BUCKET_NAME',
-		// "AWS_S3_CALLBACK_URL",
 		'AWS_S3_REGION',
 		'AWS_S3_ACCESS_KEY_ID',
 		'AWS_S3_SECRET_ACCESS_KEY',
