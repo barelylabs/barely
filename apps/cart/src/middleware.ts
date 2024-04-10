9f02421121271e391070507c9e79f6494ea75947
adambarito <adam@barely.io>
2024-04-09 21:14:03 -0400

    Ignore more middleware paths

--- apps/cart/src/middleware.ts
+++ apps/cart/src/middleware.ts
@@ -26,6 +26,6 @@ export const config = {
 		 * - _next/image (image optimization files)
 		 * - favicon.ico (favicon file)
 		 */
-		'/((?!api|_next/static|_next/image|favicon.ico).*)',
+		'/((?!api|_next|_static|.well-known|favicon|logos|sitemap|site.webmanifest).*)',
 	],
 };
