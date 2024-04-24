1252688f6f22929d101be441e04ab6204cf71fc6
adambarito <adam@barely.io>
2024-04-24 14:36:26 -0400

    use spotify.app.link for spotify deep linking on mobile (#81)
    * use spotify.app.link & log errors in link middleware
    
    * parse transparent link w/o search params
    
    only spotify can be transparent links for now, due to some youtube links having search params as part of the destination data. will re-add in a later update.
    
    * check for handle query param in preview
    
    * match any string for preview deployment id (link parsing)
    
    * use spotify.app.link on mobile
    
    * logging mobile spotify

--- apps/link/src/middleware.ts
+++ apps/link/src/middleware.ts
@@ -116,12 +116,13 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 	if (ua.device === 'mobile') {
 		if (link.url.includes('open.spotify.com')) {
 			console.log('Spotify link detected on mobile');
-			const spotifyAppLink = new URL(link.url);
+			const spotifyAppLink = new URL('https://spotify.app.link');
 			spotifyAppLink.searchParams.set('product', 'open');
 			spotifyAppLink.searchParams.set('$full_url', link.url);
 			spotifyAppLink.searchParams.set('$fallback_url', link.url);
 			spotifyAppLink.searchParams.set('$android_redirect_timeout', '3000');
-			link.url = spotifyAppLink.toString();
+			console.log('redirecting to mobile spotify', spotifyAppLink.href);
+			return NextResponse.redirect(spotifyAppLink.href);
 		}
 	}
 
