63b3b357372522bea8a024904d65a3394b9c00c8
adambarito <adam@barely.io>
2024-05-14 16:05:27 -0400

    Revert drizzle-kit to 20.17
    Getting errors with composite keys (known issue that's hopefully getting patched soon)
    
    https://discord.com/channels/1043890932593987624/1151920448049782915/1239484266879127553

--- packages/lib/server/db/drizzle.config.ts
+++ packages/lib/server/db/drizzle.config.ts
@@ -1,27 +1,27 @@
-// import type { Config } from 'drizzle-kit';
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
+// import { defineConfig } from 'drizzle-kit';
 import { z } from 'zod';
 
 const DATABASE_URL = z.string().url().parse(process.env.DATABASE_URL);
 
 const connectionString = DATABASE_URL + '?ssl=true&sslmode=require';
 
-// export default {
-//     dialect: 'pg',
-// 	schema: './server/**/*.sql.ts',
-// 	out: './server/db/_migrations',
-// 	driver: 'pg',
-// 	dbCredentials: {
-// 		connectionString,
-// 	},
-// } satisfies Config;
-
-export default defineConfig({
-	dialect: 'postgresql',
+export default {
+	// dialect: 'pg',
 	schema: './server/**/*.sql.ts',
 	out: './server/db/_migrations',
-	// driver: 'pg',
+	driver: 'pg',
 	dbCredentials: {
-		url: connectionString,
+		connectionString,
 	},
-});
+} satisfies Config;
+
+// export default defineConfig({
+// 	dialect: 'postgresql',
+// 	schema: './server/**/*.sql.ts',
+// 	out: './server/db/_migrations',
+// 	// driver: 'pg',
+// 	dbCredentials: {
+// 		url: connectionString,
+// 	},
+// });
