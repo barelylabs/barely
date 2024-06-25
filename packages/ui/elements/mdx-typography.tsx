d9ca62e4d502b28bf88ee347b73890cdd364d48a
adambarito <adam@barely.io>
2024-06-24 21:43:22 -0400

    center & wrap-balance all typography by default (#115)

--- packages/ui/elements/mdx-typography.tsx
+++ packages/ui/elements/mdx-typography.tsx
@@ -1,18 +1,63 @@
 import type { ReactNode } from 'react';
 
 import { H, Text } from './typography';
+import { WrapBalancer } from './wrap-balancer';
 
 export const mdxTypography = {
 	p: ({ children }: { children?: ReactNode }) => (
-		<Text variant='lg/normal' className='text-center'>
-			{children}
-		</Text>
+		<WrapBalancer ratio={0}>
+			<Text variant='lg/normal' className='text-center'>
+				{children}
+			</Text>
+		</WrapBalancer>
 	),
-	hero: ({ children }: { children?: ReactNode }) => <H size='hero'>{children}</H>,
-	title: ({ children }: { children?: ReactNode }) => <H size='title'>{children}</H>,
-	h1: ({ children }: { children?: ReactNode }) => <H size='1'>{children}</H>,
-	h2: ({ children }: { children?: ReactNode }) => <H size='2'>{children}</H>,
-	h3: ({ children }: { children?: ReactNode }) => <H size='3'>{children}</H>,
-	h4: ({ children }: { children?: ReactNode }) => <H size='4'>{children}</H>,
-	h5: ({ children }: { children?: ReactNode }) => <H size='5'>{children}</H>,
+	hero: ({ children }: { children?: ReactNode }) => (
+		<WrapBalancer>
+			<H size='hero' className='text-center'>
+				{children}
+			</H>
+		</WrapBalancer>
+	),
+	title: ({ children }: { children?: ReactNode }) => (
+		<WrapBalancer>
+			<H size='title' className='text-center'>
+				{children}
+			</H>
+		</WrapBalancer>
+	),
+	h1: ({ children }: { children?: ReactNode }) => (
+		<WrapBalancer>
+			<H size='1' className='text-center'>
+				{children}
+			</H>
+		</WrapBalancer>
+	),
+	h2: ({ children }: { children?: ReactNode }) => (
+		<WrapBalancer>
+			<H size='2' className='text-center'>
+				{children}
+			</H>
+		</WrapBalancer>
+	),
+	h3: ({ children }: { children?: ReactNode }) => (
+		<WrapBalancer>
+			<H size='3' className='text-center'>
+				{children}
+			</H>
+		</WrapBalancer>
+	),
+	h4: ({ children }: { children?: ReactNode }) => (
+		<WrapBalancer>
+			<H size='4' className='text-center'>
+				{children}
+			</H>
+		</WrapBalancer>
+	),
+	h5: ({ children }: { children?: ReactNode }) => (
+		<WrapBalancer>
+			<H size='5' className='text-center'>
+				{children}
+			</H>
+		</WrapBalancer>
+	),
 };
