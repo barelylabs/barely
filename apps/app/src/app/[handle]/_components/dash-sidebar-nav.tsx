d33392f1a85cf34a9774ebe663d0794b1cee4d78
adambarito <adam@barely.io>
2024-06-26 17:05:02 -0400

    add fm to sidebar (#118)

--- apps/app/src/app/[handle]/_components/dash-sidebar-nav.tsx
+++ apps/app/src/app/[handle]/_components/dash-sidebar-nav.tsx
@@ -66,7 +66,8 @@ export function SidebarNav(props: { workspace: Workspace }) {
 
 	const pageLinks: SidebarNavLink[] = [
 		{ title: 'links', icon: 'link', href: `/${handle}/links` },
-		{ title: 'bio', icon: 'bio', href: `/${handle}/bio` },
+		{ title: 'fm', icon: 'fm', href: `/${handle}/fm` },
+		// { title: 'bio', icon: 'bio', href: `/${handle}/bio` },
 		{ title: 'pages', icon: 'landingPage', href: `/${handle}/pages` },
 		{ title: 'press', icon: 'press', href: `/${handle}/press` },
 	];
