ab8e75b2458d7118606b97e42ad5a5c46c62bf24
adambarito <adam@barely.io>
2024-09-04 01:02:32 -0400

    improved email domain error reporting (#149)
    * improved email domain error reporting
    
    * fix logic in email domain verification

--- packages/lib/server/routes/email-domain/email-domain.router.ts
+++ packages/lib/server/routes/email-domain/email-domain.router.ts
@@ -1,5 +1,6 @@
 import { resend } from '@barely/email';
 import { tasks } from '@trigger.dev/sdk/v3';
+import { TRPCError } from '@trpc/server';
 import { and, asc, desc, eq, gt, lt, or } from 'drizzle-orm';
 import { z } from 'zod';
 
@@ -18,8 +19,6 @@ import {
 } from './email-domain.schema';
 import { EmailDomains } from './email-domain.sql';
 
-// import type {verifyEmailDomain} from '../'
-
 export const emailDomainRouter = createTRPCRouter({
 	create: privateProcedure
 		.input(createEmailDomainSchema)
@@ -31,8 +30,30 @@ export const emailDomainRouter = createTRPCRouter({
 			});
 
 			if (domainRes.error) {
-				throw new Error(domainRes.error.message);
-			} else if (!domainRes.data) {
+				switch (domainRes.error.name) {
+					case 'validation_error': {
+						throw new TRPCError({
+							code: 'FORBIDDEN',
+							message: `Resend validation error: ${domainRes.error.message}`,
+						});
+					}
+
+					case 'rate_limit_exceeded': {
+						throw new TRPCError({
+							code: 'TOO_MANY_REQUESTS',
+							message: `Resend rate limit exceeded: ${domainRes.error.message}`,
+						});
+					}
+					default: {
+						throw new TRPCError({
+							code: 'INTERNAL_SERVER_ERROR',
+							message: `Resend error: ${domainRes.error.name} - ${domainRes.error.message}`,
+						});
+					}
+				}
+			}
+
+			if (!domainRes.data) {
 				throw new Error('No data returned from Resend');
 			}
 
@@ -83,19 +104,19 @@ export const emailDomainRouter = createTRPCRouter({
 				),
 			});
 
-			console.log('domain', domain);
-
 			if (!domain) {
 				throw new Error('Domain not found');
 			}
 
-			if (domain.status !== 'pending' && !input.force) {
-				// if the domain is already verified and we're not forcing a re-verification, return the domain
+			// if the domain is already verified and we're not forcing a re-verification, return the domain
+			if (domain.status === 'verified' && !input.force) {
 				return domain;
 			}
 
+			// if we're not forcing a re-verification, check if the domain is already verified on Resend
 			if (!input.force) {
 				const currentDomainRes = await resend.domains.get(domain.resendId);
+
 				if (currentDomainRes.data?.status === 'verified') {
 					const updatedDomain = await ctx.db.pool
 						.update(EmailDomains)
@@ -109,37 +130,19 @@ export const emailDomainRouter = createTRPCRouter({
 				}
 			}
 
+			/**
+			 * we're either forcing a re-verification or the domain is not verified on Resend.
+			 * in either case, we need to update the domain to pending and trigger a verification
+			 */
+
+			await ctx.db.pool
+				.update(EmailDomains)
+				.set({
+					status: 'pending',
+				})
+				.where(eq(EmailDomains.id, domain.id));
+
 			await tasks.trigger<typeof verifyEmailDomain>('verify-email-domain', domain);
-
-			// const verifyRes = await resend.domains.verify(domain.resendId);
-
-			// if (verifyRes.error) {
-			// 	throw new Error(verifyRes.error.message);
-			// } else if (!verifyRes.data) {
-			// 	throw new Error('No data returned from Resend');
-			// }
-
-			// // await wait(15 * 1000) // wait for 15 seconds to allow the domain to be verified
-
-			// const updatedDomainRes = await resend.domains.get(domain.resendId);
-
-			// console.log('updatedDomainRes', updatedDomainRes);
-			// console.log('updatedDomainRes.records', updatedDomainRes.data?.records);
-
-			// if (!updatedDomainRes?.data) {
-			// 	throw new Error('No data returned from Resend');
-			// }
-
-			// const updatedDomain = await ctx.db.pool
-			// 	.update(EmailDomains)
-			// 	.set({
-			// 		status: updatedDomainRes.data.status,
-			// 		records: updatedDomainRes.data.records,
-			// 	})
-			// 	.where(eq(EmailDomains.id, domain.id))
-			// 	.returning();
-
-			// return updatedDomain;
 		}),
 
 	byName: workspaceQueryProcedure.input(z.string()).query(async ({ ctx, input }) => {
