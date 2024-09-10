057b7d37583f83525c6890a8d68d5104a9504799
adambarito <adam@barely.io>
2024-09-10 12:28:58 -0400

    remove key from funnelWith

--- packages/lib/server/routes/cart/cart.fns.ts
+++ packages/lib/server/routes/cart/cart.fns.ts
@@ -28,7 +28,7 @@ import { getAmountsForCheckout, getFeeAmountForCheckout } from './cart.utils';
 /* get funnel */
 export const funnelWith = {
 	workspace: true,
-	key: true,
+	// key: true,
 	mainProduct: {
 		with: {
 			_images: {
