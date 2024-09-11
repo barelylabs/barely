122dc9e0a1f40bf21e993ea05cd2047f04afb8a1
adambarito <adam@barely.io>
2024-09-11 17:33:55 -0400

    pass thru fanId && fbclid from landingPage Link && AssetButtons

--- apps/page/src/app/[handle]/[...key]/page.tsx
+++ apps/page/src/app/[handle]/[...key]/page.tsx
@@ -3,8 +3,10 @@ import type { LandingPage } from '@barely/lib/server/routes/landing-page/landing
 import type { Link } from '@barely/lib/server/routes/link/link.schema';
 import type { PressKit } from '@barely/lib/server/routes/press-kit/press-kit.schema';
 import type { Metadata } from 'next';
+import type { z } from 'zod';
+import { eventReportSearchParamsSchema } from '@barely/lib/server/routes/event/event-report.schema';
 import { getLandingPageData } from '@barely/lib/server/routes/landing-page/landing-page.render.fns';
-import { getAssetHref } from '@barely/lib/utils/mdx';
+import { getAssetHref, getLinkHref } from '@barely/lib/utils/mdx';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import { mdxTypography } from '@barely/ui/elements/mdx-typography';
@@ -43,10 +45,16 @@ export async function generateMetadata({
 
 export default async function LandingPage({
 	params,
+	searchParams,
 }: {
 	params: { handle: string; key: string[] };
+	searchParams: z.infer<typeof eventReportSearchParamsSchema>;
 }) {
 	const key = params.key.join('/');
+
+	const searchParamsSafe = eventReportSearchParamsSchema.safeParse(searchParams);
+
+	const { fbclid, fanId } = searchParamsSafe.data ?? {};
 
 	const data = await getLandingPageData({
 		handle: params.handle,
@@ -69,12 +77,16 @@ export default async function LandingPage({
 					...mdxVideoPlayer,
 					...mdxAssetButton({
 						landingPageId: lp.id,
+						// assets
 						cartFunnels,
 						links,
 						pressKits,
 						landingPages,
+						// tracking
+						fbclid,
+						fanId,
 					}),
-					...mdxLinkButton({ landingPageId: lp.id }),
+					...mdxLinkButton({ landingPageId: lp.id, fbclid, fanId }),
 				}}
 			/>
 			<LogVisit landingPageId={lp.id} />
@@ -82,12 +94,32 @@ export default async function LandingPage({
 	);
 }
 
-function mdxLinkButton({ landingPageId }: { landingPageId: string }) {
-	const LinkButton = ({ href, label }: { href: string; label: string }) => (
-		<div className='flex w-full flex-col items-center'>
-			<LandingPageLinkButton landingPageId={landingPageId} href={href} label={label} />
-		</div>
-	);
+function mdxLinkButton({
+	landingPageId,
+	fbclid,
+	fanId,
+}: {
+	landingPageId: string;
+	fbclid?: string;
+	fanId?: string;
+}) {
+	const LinkButton = (props: { href: string; label: string }) => {
+		const href = getLinkHref({
+			href: props.href,
+			refererId: landingPageId,
+			fbclid,
+			fanId,
+		});
+		return (
+			<div className='flex w-full flex-col items-center'>
+				<LandingPageLinkButton
+					landingPageId={landingPageId}
+					href={href}
+					label={props.label}
+				/>
+			</div>
+		);
+	};
 
 	return {
 		LinkButton,
@@ -100,12 +132,18 @@ function mdxAssetButton({
 	landingPages,
 	links,
 	pressKits,
+	// tracking
+	fbclid,
+	fanId,
 }: {
 	landingPageId: string;
 	cartFunnels: CartFunnel[];
 	landingPages: LandingPage[];
 	links: Link[];
 	pressKits: PressKit[];
+	// tracking
+	fbclid?: string;
+	fanId?: string;
 }) {
 	const AssetButton = ({ assetId, label }: { assetId: string; label: string }) => {
 		const href = getAssetHref({
@@ -115,6 +153,8 @@ function mdxAssetButton({
 			links,
 			pressKits,
 			refererId: landingPageId,
+			fbclid,
+			fanId,
 		});
 
 		return (
