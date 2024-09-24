1b0a92588988a2b0cd8b5f7d93eb554376c2a762
adambarito <adam@barely.io>
2024-09-24 00:42:15 -0400

    added cors options to unsubscribe/[emailDeliveryId] route (#193)

--- apps/manage-email/src/app/unsubscribe/[emailDeliveryId]/route.ts
+++ apps/manage-email/src/app/unsubscribe/[emailDeliveryId]/route.ts
@@ -1,12 +1,15 @@
 import { emailManageApi } from '@barely/lib/server/routes/email-manage/email-manage.api.server';
+import { OPTIONS } from '@barely/lib/utils/trpc-route';
 import { getAbsoluteUrl } from '@barely/lib/utils/url';
 import { TRPCError } from '@trpc/server';
 
-export async function POST(
+async function POST(
 	request: Request,
 	{ params }: { params: { emailDeliveryId: string } },
 ) {
 	const { emailDeliveryId } = params;
+
+	console.log('unsubscribing from emailDeliveryId', emailDeliveryId);
 
 	try {
 		await emailManageApi.toggleEmailMarketingOptIn({
@@ -14,21 +17,22 @@ export async function POST(
 			forceUnsubscribe: true,
 		});
 
-		// return Response.redirect(
-		// 	getAbsoluteUrl('manageEmail', `/manage/${emailDeliveryId}?unsubscribed=true`),
-		// );
+		console.log('unsubscribed');
+
 		return Response.json({}, { status: 200 });
 	} catch (error) {
 		if (error instanceof TRPCError) {
+			console.error(error);
 			return Response.json({ error: error.message }, { status: 500 });
 		}
 
+		console.error(error);
 		return Response.json({ error: 'Something went wrong' }, { status: 500 });
 	}
 }
 
 /* GET for webcrawlers of POST Header */
-export async function GET(
+async function GET(
 	request: Request,
 	{ params }: { params: { emailDeliveryId: string } },
 ) {
@@ -39,3 +43,5 @@ export async function GET(
 	// return Response.redirect(`https://${domain}/${emailDeliveryId}`);
 	return Response.redirect(getAbsoluteUrl('manageEmail', `/manage/${emailDeliveryId}`));
 }
+
+export { OPTIONS, GET, POST };
